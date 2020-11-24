13a7866f340f7d278c78bcc0979ce679490aacd6
Documenter.jl <documenter@juliadocs.github.io>
2020-11-24 07:54:00 +0000

    build based on f4b7a36

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Ptychography","category":"page"},{"location":"#Ptychography","page":"Home","title":"Ptychography","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Ptychography]","category":"page"}]
+}
